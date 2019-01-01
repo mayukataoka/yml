32710f22d92fb81ac64416151a3eee81580ffe5b
MAYU KATAOKA <mayukataoka@MAYUs-MacBook-Pro.local>
2019-01-01 14:13:39 -0800

    yml

---
+++ number_pyramid.py
@@ -0,0 +1,21 @@
+import sys
+
+def create_new_pyramid_row(input_list):
+    i = 0
+    new_row = []
+    while i + 1 < len(input_list):
+        new_number = input_list[i] + input_list[i+1]
+        new_row.append(new_number)
+        i += 1
+    return new_row
+
+def create_pyramid_vertically(input_list):
+
+    row = input_list
+    for i in range(0, len(input_list)):
+        print(*row)
+        row = create_new_pyramid_row(row)
+
+if __name__ == '__main__':
+    given_list = [int(i) for i in sys.argv[1:]]
+    create_pyramid_vertically(given_list)
